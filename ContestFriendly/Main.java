cd54c917fe97abf6e95b1c0e9468ccc9bcab75fd
someeranmahata <someeranmahata@gmail.com>
2026-05-03 12:52:48 +0530

    file changes

--- ContestFriendly/Main.java
+++ ContestFriendly/Main.java
@@ -2,22 +2,51 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static long[] dp = new long[2_00_000];
 
     public static String solve(Scanner sc) {
         
-        StrinBuilder st = new StringBuilder(st);
+        StringBuilder st = new StringBuilder();
 
         int test = sc.nextInt();
-        while(test-- > 0){
-                
+        for(int tt = 1; tt <= test; tt++){
+            
+            int n = sc.nextInt();
+            long[][] ar = new long[n][2];
+            
+            for(int i = 0; i < n; i++){
+                ar[i][0] = sc.nextLong();
+                ar[i][1] = (long)(i + 1);
+            }
+            for(int i = 0; i < n; i++)
+                ar[i][0] -= sc.nextLong();
+
+            Arrays.sort(ar, (a, b)->Long.compare(b[0], a[0]));
+
+            int count = 0;
+            StringBuilder vertex = new StringBuilder();
+            int i = 0;
+            while(i < n && ar[0][0] == ar[i][0]){
+                count++;
+                vertex.append(ar[i][1] + " ");
+                i++;
+            }
+            st.append(count + "\n");
+            st.append(vertex + "\n");
+
         }
 
 
-        System.out.prinltn(st.toString());
-
+        return st.toString();
     }
     
-    
+    static long fact(int n){
+        if(n <= 1)
+            return 1;
+        if(dp[n] != 0)
+            return dp[n];
+        return dp[n] = (n*fact(n - 1))%998244353;
+    }
     //METHODS
     static long gcd(long a, long b) {
         while (b != 0) {
